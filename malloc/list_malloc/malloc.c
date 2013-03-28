c2860f63ecfc3a8a4bb35af4ce9f4a3d401d0c9d
Andrey Komarov <taukus@gmail.com>
2013-03-28 19:38:13 +0400

    something works

--- malloc/list_malloc/malloc.c
+++ malloc/list_malloc/malloc.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 
+#include <assert.h>
+
 #include "malloc.h"
 
 #define PAGE_SIZE 4096
@@ -10,109 +12,164 @@
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-// size --- без учёта себя
-typedef struct free_block_t
-{
-    size_t size;
-    struct free_block_t * next;
-} free_block_t;
+#define BLK_FREE 0x1
+#define BLK_BOUND 0x2
 
-// size --- без учёта себя
-typedef struct occupied_block_t
+/* Что происходит?
+ *
+ * Есть суперблоки. В каждом суперблоке лежат блоки. Самый левый и самый правый
+ * блоки суперблока --- служебные. По блоку можно получить левого и правого 
+ * соседей.
+ *
+ * Свободные блоки объединены в двусвязный список. Когда просят malloc,
+ * в списле ищется первый, имеющий достаточный размер. Когда делают free, у 
+ * блока смотрятся соседи, и, если они свободны, то они выпиливаются из списка, 
+ * а сосед сливается с удаляемым. 
+ */ 
+
+typedef struct block_t
 {
-    size_t size;
+    struct block_t * prev;
+    struct block_t * next;
+    struct block_t * prev_in_superblock;
+    size_t size; // Чистый размер. Вот столько можно дать памяти
+    int flags;
     char elems[];
-} occupied_block_t;
+} block_t;
 
-static free_block_t * head = NULL;
+static block_t * head = NULL;
 
-void print_freelist(free_block_t * block)
+void set_head(block_t * block)
 {
-    if (block == NULL)
-        printf ("[]\n");
-    else
+    block->next = head;
+    head = block;
+    head->prev = NULL;
+}
+
+
+block_t * init_bounding_block(void * ptr)
+{
+    block_t * block = (block_t*) ptr;
+    block->flags = BLK_BOUND;
+    return block;
+}
+
+block_t * new_superblock(size_t size)
+{
+    assert (size > 3 * sizeof(block_t));
+    void * ptr = mmap(NULL, size, PROT_READ | PROT_WRITE,
+            MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    char * cptr = (char*)ptr;
+    block_t * left_bound = init_bounding_block(ptr);
+    init_bounding_block(cptr + size - sizeof(block_t));
+    block_t * main_block = (block_t*)(cptr + sizeof(block_t));
+    main_block->prev = NULL;
+    main_block->next = NULL;
+    main_block->size = size - 3 * sizeof(block_t);
+    main_block->prev_in_superblock = left_bound;
+    main_block->flags = BLK_FREE;
+    return main_block;
+}
+
+block_t * next_in_superblock(block_t * block)
+{
+    char* ptr = (char*)block;
+    return (block_t*)(ptr + sizeof(block_t) + block->size);
+}
+
+void print_my_superblock(block_t * block)
+{
+    block_t * cur = block;
+    while (cur->prev_in_superblock != NULL)
+        cur = cur->prev_in_superblock;
+    cur = next_in_superblock(cur);
+    printf("(%x)[", block);
+    while (!(cur->flags & BLK_BOUND))
     {
-        printf("at %x free %lu :: ", block, block->size);
-        print_freelist(block->next);
+        if (cur->flags & BLK_FREE)
+            printf("F %lu; ", cur->size);
+        cur = next_in_superblock(cur);
+    }
+    printf("]\n");
+}
+
+void print_freelist()
+{
+    printf("free[");
+    block_t * cur = head;
+    while (cur != NULL)
+    {
+        printf("%lu ", cur->size);
+        cur = cur->next;
+    }
+    printf("]\n");
+}
+
+void attach(block_t * block)
+{
+    if (head != NULL)
+        head->prev = block;
+    set_head(block);
+}
+
+void remove_from_list(block_t* block)
+{
+    if (block->prev != NULL)
+        block->prev->next = block->next;
+    if (block->next != NULL)
+        block->next->prev = block->prev;
+    if (block == head)
+        head = block->next;
+}
+
+// Откусить от блока кусок, достаточный для выделения size байт и положить его
+// в голову списка
+void shrink(block_t * block, size_t size)
+{
+    // Если можем разбить на две части... 
+    if (block->size > size + sizeof(block_t))
+    {
+        remove_from_list(block);
+        char * begin1 = (char*)block;
+        char * begin2 = begin1 + sizeof(block_t) + size;
+        block_t * b1 = (block_t*)begin1;
+        block_t * b2 = (block_t*)begin2;
+        b1->prev_in_superblock = block->prev_in_superblock;
+        b2->prev_in_superblock = b1;
+        b1->flags = BLK_FREE;
+        b2->flags = BLK_FREE;
+        b2->size = block->size - sizeof(block_t) - size;
+        b1->size = size;
+        set_head(b2);
+        set_head(b1);
+    }
+    else // Иначе --- отдаём целиком
+    {
+        remove_from_list(block);
+        set_head(block);
     }
 }
 
-occupied_block_t * get_occ(void * ptr)
+block_t* block_by_addr(void* ptr)
 {
-    return (occupied_block_t*)(ptr - sizeof(occupied_block_t));
-}
-
-size_t get_size(void * ptr)
-{
-    return get_occ(ptr)->size;
-}
-
-// округлить вверх до кратного PAGE_SIZE
-size_t adjust(size_t size)
-{
-    size += sizeof(occupied_block_t) + sizeof(free_block_t);
-    size = (size / PAGE_SIZE + 1) * PAGE_SIZE;
-    return size;
-}
-
-// откусим в конце current себе occupied_block_t
-occupied_block_t * shrink(free_block_t* current, size_t size)
-{
-    char * begin = (char*)current;
-    size_t new_size = current->size - size - sizeof(occupied_block_t);
-    current->size = new_size;
-    occupied_block_t * res = (occupied_block_t*)(begin + new_size + sizeof(free_block_t));
-    res->size = size;
-    return res;
-}
-
-// Находит первый блок, в который можно утолкать size байт
-free_block_t * find_large_enough(size_t size)
-{
-    free_block_t * current = head;
-    while (current != NULL)
-    {
-        if (current->size >= size + sizeof(occupied_block_t) + sizeof(free_block_t))
-            return current;
-        current = current->next;
-    }
-    return NULL;
-}
-
-// выделить блок, в который влезет size байт
-free_block_t * new_block(size_t size)
-{
-    size_t bsz = adjust(size);
-    void * ptr = mmap(NULL, bsz, PROT_READ | PROT_WRITE, 
-            MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    free_block_t * new = (free_block_t*)ptr;
-    new->size = bsz - sizeof(free_block_t);
-    new->next = NULL;
-    return new;
+    char* cptr = (char*)ptr;
+    return (block_t*)(cptr - sizeof(block_t));
 }
 
 void * malloc(size_t size)
 {
-    free_block_t * current = find_large_enough(size);
-    if (current == NULL)
-    {
-        free_block_t * new = new_block(size);
-        new->next = head;
-        current = head = new;
-    }
-    void * res = shrink(current, size)->elems;
-    return res;
+    return NULL;
 }
 
-void free(void * ptr) {
-    return;
+void free(void * ptr)
+{
 }
 
 void * realloc(void * ptr, size_t size) {
     if (ptr == NULL)
         return NULL;
     void * ret_ptr = malloc(size);
-    memcpy(ret_ptr, ptr, MIN(get_size(ptr), size));
+    memcpy(ret_ptr, ptr, MIN(block_by_addr(ptr)->size, size));
     free(ptr);
     return ret_ptr;
 }
@@ -129,24 +186,19 @@ void crash() {
     _exit(1);
 }
 
+/*
 int posix_memalign(void **memptr, size_t alignment, size_t size) { crash(); }
 void *aligned_alloc(size_t alignment, size_t size) { crash(); }
 void *valloc(size_t size) { crash(); }
 void *memalign(size_t alignment, size_t size) { crash(); }
 void *pvalloc(size_t size) {crash(); }
-
-/*
+*/
 
 int main()
 {
-    int i;
-    for (i = 0; i < 10000; i++)
-    {
-        int * a = (int*)(malloc(sizeof(int)));
-        *a = i;
-    //    printf("%d ", *a);
-    }
+    block_t* b = new_superblock(PAGE_SIZE);
+    shrink(b, 1000);
+    shrink(head, 200);
+    print_my_superblock(b);
     return 0;
 }
-
-*/
