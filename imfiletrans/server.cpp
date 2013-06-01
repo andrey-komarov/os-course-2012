d8cb4d17549bee8c13a66988b2924bfa3a890bd5
Andrey Komarov <taukus@gmail.com>
2013-06-01 17:09:19 +0400

    SENDER works

--- imfiletrans/server.cpp
+++ imfiletrans/server.cpp
@@ -13,25 +13,53 @@
 
 using namespace std;
 
+char* itoa(int n, char* buf)
+{
+    int len = 0;
+    int nn = n;
+    while (nn)
+    {
+        nn /= 10;
+        len++;
+    }
+    buf[len] = 0;
+    while (n)
+    {
+        buf[--len] = n % 10 + '0';
+        n /= 10;
+    }
+    return buf;
+}
+
 struct Event;
 map<int, Event*> senders;
-map<int, Event*> receivers;
 
 #define BUFSIZE (32 * (1 << 20))
 struct Event 
 {
     enum Type { UNDEF, SENDER, RECEIVER };
+    int epollfd;
     int fd;
     Type type;
-    char buf[BUFSIZE];
+    char *buf;
     int bufpos;
     int token;
+    bool done;
+    Event* dual;
+    epoll_event ev;
+    bool needSend;
+   
+    char outbuf[20];
+    int outbufNeed;
+    int outbufWritten;
 
     Event() {}
 
-    Event(int fd) 
-        : fd(fd)
+    Event(int epollfd, int fd)
+        : epollfd(epollfd)
+        , fd(fd)
         , type(UNDEF)
+        , buf(new char[BUFSIZE])
         , bufpos(0)
     {}
 
@@ -47,20 +75,102 @@ struct Event
         bufpos += n;
         if (bufpos >= 5)
         {
-            if (buf[0] == 's' && buf[1] == 'e' && buf[2] == 'n' && buf[3] == 'd' && buf[4] == '\n')
+            if (strncmp("send\n", buf, 5) == 0)
             {
                 type = SENDER;
+                memmove(buf + 5, buf, bufpos - 5);
+                bufpos -= 5;
                 token = rand();
+                senders[token] = this;
+                done = false;
+
+                itoa(token, outbuf);
+                outbufNeed = strlen(outbuf);
+                outbufWritten = 0;
+                ev.events |= EPOLLOUT;
             }
+            else if (strncmp("recv\n", buf, 5) == 0)
+            {
+                type = RECEIVER;
+                memmove(buf + 5, buf, bufpos - 5);
+                bufpos -= 5;
+                done = false;
+            }
+            else
+            {
+                printf("FAIL: don't know what to do with <%s>\n", buf);
+                exit(1);
+            }
+        }
+    }
+
+    void runSender()
+    {
+        printf("events = %d IN = %d OUT = %d\n", ev.events, EPOLLIN, EPOLLOUT);
+        if (!done)
+        {
+            int n = read(fd, buf, BUFSIZE - bufpos);
+            bufpos += n;
+            if (n == 0) 
+            {
+                done = true;
+                ev.events &=~ EPOLLIN;
+                epoll_ctl(epollfd, EPOLL_CTL_MOD, fd, &ev);
+                printf("done here(%d)\n", fd);
+                needSend = true;
+            }
+        }
+        else
+        {
+        }
+
+        if ((ev.events & EPOLLOUT) && outbufWritten < outbufNeed)
+        {
+            int n = write(fd, outbuf + outbufWritten, outbufNeed - outbufWritten);
+            outbufWritten += n;
+            if (outbufWritten == outbufNeed)
+            {
+                ev.events &=~ EPOLLOUT;
+            }
+        }
+        if (ev.events == 0)
+        {
+            epoll_ctl(epollfd, EPOLL_CTL_DEL, fd, &ev);
+            delete[](buf);
+            close(fd);
+        }
+    }
+
+    void runReceiver()
+    {
+        if (!done)
+        {
+            int n = read(fd, buf, BUFSIZE - bufpos);
+            bufpos += n;
+            if (n == 0)
+            {
+                done = true;
+                token = atoi(buf);
+                printf("receive token %d\n", token);
+            }
+        }
+        else
+        {
+
         }
     }
 
     void run()
     {
+        printf("running %d\n", fd);
         if (type == UNDEF)
             runUndef();
+        else if (type == SENDER)
+            runSender();
+        else if (type == RECEIVER)
+            runReceiver();
     }
-}
+};
 
 int main()
 {
@@ -146,22 +256,18 @@ int main()
                     continue;
                 }
                 printf("accepted, %d\n", newfd);
-                printf("%d\n", rand());
-                int pid = fork();
-                if (pid == 0)
+                Event* e = new Event(epollfd, newfd);
+                e->ev.events = EPOLLIN;
+                e->ev.data.ptr = e;
+                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, newfd, &e->ev) < 0)
                 {
-                    close(sockfd);
-                    dup2(newfd, 0);
-                    dup2(newfd, 1);
-//                    dup2(newfd, 2);
-                    close(newfd);
-                    char BUF[1000];
-                    int n = read(0, BUF, 1000);
-                    write(2, BUF, n);
-                    execlp("echo", "echo", "hello", NULL);
-                    exit(0);
+                    perror("epoll_ctl");
+                    exit(1);
                 }
-                close(newfd);
+            }
+            else 
+            {
+                ((Event*)events[i].data.ptr)->run();
             }
         }
 
